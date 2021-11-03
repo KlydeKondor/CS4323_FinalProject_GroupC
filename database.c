a1ab19e168138c423366cf88ce93e7ec3097899e
Kyle Kentner <kyle.kentner@okstate.edu>
2021-11-03 14:09:30 -0500

    Removed errors; still needs driver, file I/O testing

--- database.c
+++ database.c
@@ -13,10 +13,12 @@
 #define DELETE 4
 
 // Column constants
+#define PRODUCT_ID 0 // Quantity available (productInformation)
+
 #define QUANTITY_A 3 // Quantity available (productInformation)
 #define QUANTITY_P 2 // Quantity purchased (customerOrder)
 
-int database(int cmdType, int col, const char* tbl, const char* setVal, const char* whereCol, const char* whereVal) {
+int database(int cmdType, int col, const char* tbl, char* setVal, int whereCol, char* whereVal) {
 	int rowsAffected = 0;
 	
 	// File pointer for the client's table
@@ -25,15 +27,19 @@ int database(int cmdType, int col, const char* tbl, const char* setVal, const ch
 	switch (cmdType) {
 	case 1: // SELECT
 		fDB = fopen(tbl, "r");
+		fDB.close();
 		break;
 	case 2: // INSERT
 		fDB = fopen(tbl, "a");
+		fDB.close();
 		break;
 	case 3: // UPDATE
 		fDB = fopen(tbl, "r+");
+		fDB.close();
 		break;
 	case 4: // DELETE
 		fDB = fopen(tbl, "r+");
+		fDB.close();
 		break;
 	default:
 		rowsAffected = -1;
@@ -74,13 +80,16 @@ int updateQuantityPrice() {
 	return updateSuccess;
 }
 
-int purchaseReturnProduct(int change) {
+int purchaseReturnProduct(char* productID, char* customerID, char* orderID, char* change) {
 	int purchaseReturnSuccess = 0;
+	
+	// If able to read, get the current quantity available
+	// TODO: Get value from database, see if purchase can be made
+	purchaseReturnSuccess = database(SELECT, QUANTITY_A, "productInformation.txt", PRODUCT_ID, productID); 
 	
 	// Update the quantity of a product in productInformation; reflect changes in billingInformation and customerOrder
 	// TODO: Check if client has permission to access productInformation, billingInformation, and customerOrder
-	purchaseReturnSuccess = database(UPDATE, QUANTITY_A, "productInformation.txt",  
-	purchaseReturnSuccess = database(UPDATE, QUANTITY_A, "productInformation.txt", 
+	purchaseReturnSuccess = database(UPDATE, QUANTITY_A, "productInformation.txt", PRODUCT_ID, productID);
 	
 	return purchaseReturnSuccess;
 }
