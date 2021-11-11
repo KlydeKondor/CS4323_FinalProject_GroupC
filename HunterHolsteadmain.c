03050128b7e4b37b04a4ab7fa4dd767256656ebe
HunterHolstead <74698387+HunterHolstead@users.noreply.github.com>
2021-11-10 20:49:08 -0600

    Add files via upload
    HunterHolsteadmain.c is an example of how to run all of the corresponding function tests with the .h file. Setters have been written for the clients in the current state however future versions will see the database functions creating structs that hold information in variables for easier access from getter methods. The overall flow of the client program will be reworked as well.

---
+++ HunterHolsteadmain.c
@@ -0,0 +1,13 @@
+//Hunter Holstead
+
+#include <stdio.h>
+#include "HunterHolstead.h"
+
+//An example of how to run function tests for HunterHolstead
+
+int main()
+{
+	RegisterClient();
+	
+	return 0;
+}
